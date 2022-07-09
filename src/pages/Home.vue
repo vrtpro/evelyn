fa33111256ba3b3bd0f527d3b012f051635d47c8
Gifaldy Azka <me@falcxxdev.ml>
2022-07-09 13:18:42 +0700

    chore: update page content

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,47 @@
 <template>
-    <h1>Hello world</h1>
+    <h1>Hello, {{ name }}</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis amet consequatur ut odio veniam laudantium,
+        reprehenderit laboriosam perspiciatis a! Temporibus eum porro labore dolorum expedita optio, odit ducimus facere
+        fugit! Voluptas tempora sint error. Quo perspiciatis tempora dolorum sit animi, vitae, recusandae odit ea et
+        nesciunt, odio quae vel iste?
+    </p>
+    <p>
+        Vue (pronounced /vjuː/, like view) is a JavaScript framework for building user interfaces. It builds on top of
+        standard HTML, CSS and JavaScript, and provides a declarative and component-based programming model that helps
+        you efficiently develop user interfaces, be it simple or complex.
+    </p>
+    <div>
+        <h2 class="fira-sans">Font Family Test</h2>
+        <h4 class="lexend">Lexend</h4>
+        <p class="lexend">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores nulla optio possimus minima aliquam
+            repudiandae illo doloribus explicabo totam reprehenderit molestiae nesciunt vero tempore, magnam cumque odio
+            tenetur vel.
+        </p>
+        <h4 class="poppins">Poppins</h4>
+        <p class="poppins">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate culpa accusantium, fugiat consectetur
+            tempore consequatur. Voluptate eveniet est a voluptatem. Pariatur eaque quas numquam ipsam maxime ducimus
+            doloremque, beatae iste.
+        </p>
+        <h4 class="fira-sans">Fira Sans</h4>
+        <p class="fira-sans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloribus dolore commodi qui,
+            facilis eum architecto veritatis, laborum iste deleniti odio consequuntur reiciendis quidem incidunt fuga
+            quis voluptatem? Corrupti, deleniti.
+        </p>
+    </div>
 </template>
 
 <script lang="ts">
 export default {
     name: 'Home',
+    data() {
+        return {
+            name: 'Epel 🍎',
+        };
+    },
 };
 </script>
 
@@ -13,5 +50,18 @@ export default {
 
 h1 {
     font-family: $lexend;
+}
+p {
+    font-family: $poppins;
+}
+
+.lexend {
+    font-family: $lexend;
+}
+.poppins {
+    font-family: $poppins;
+}
+.fira-sans {
+    font-family: $fira_sans;
 }
 </style>
