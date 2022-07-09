d14621a87c40a921a954411f9fc906f56ebd7e12
Gifaldy Azka <me@falcxxdev.ml>
2022-07-09 13:55:55 +0700

    feat: implement dynamic title on route change

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, Router } from 'vue-router';
-import './routes.d';
+import '../types/routes.d';
 
 import Home from '../pages/Home.vue';
 
@@ -8,12 +8,46 @@ const routes: Routes = [
         path: '/',
         name: 'Home',
         component: Home,
+        meta: { title: 'Home | EpelEvelyn' },
     },
 ];
 
 const router: Router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeResolve((to, from, next) => {
+    const nearestWithTitle: any = to.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.title);
+    const nearestWithMeta: any = to.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.metaTags);
+    const previousNearestWithMeta: any = from.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.metaTags);
+    if (nearestWithTitle) {
+        document.title = nearestWithTitle.meta.title;
+    } else if (previousNearestWithMeta) {
+        document.title = previousNearestWithMeta.meta.title;
+    }
+    Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+    if (!nearestWithMeta) return next();
+    nearestWithMeta.meta.metaTags
+        .map(tagDef => {
+            const tag: HTMLMetaElement = document.createElement('meta');
+            Object.keys(tagDef).forEach(key => {
+                tag.setAttribute(key, tagDef[key]);
+            });
+            tag.setAttribute('data-vue-router-controlled', '');
+            return tag;
+        })
+        .forEach(tag => document.head.appendChild(tag));
+    next();
 });
 
 export default router;
