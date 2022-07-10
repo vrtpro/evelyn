f05b02e061dc92c8c52dd51efa166f78f99bb634
BayuDC <dev.bayudc@gmail.com>
2022-07-10 12:19:33 +0700

    chore: add overlay component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,22 @@
 <script>
 import { Icon } from '@iconify/vue';
 import Container from './Container.vue';
+import Overlay from './Overlay.vue';
+import Nav from './Nav.vue';
+import Overlay from './Overlay.vue';
+
 export default {
     name: 'Header',
+    data() {
+        return {
+            open: false,
+        };
+    },
     components: {
         Container,
         Icon,
+        Nav,
+        Overlay,
     },
 };
 </script>
@@ -14,15 +25,9 @@ export default {
     <header class="abc">
         <Container>
             <h1><router-link to="#">Evelyn Logo</router-link></h1>
-            <nav>
-                <ul>
-                    <li><router-link to="#">Home</router-link></li>
-                    <li><router-link to="#">Lorem</router-link></li>
-                    <li><router-link to="#">Ipsum</router-link></li>
-                    <li><router-link to="#">About</router-link></li>
-                </ul>
-            </nav>
-            <button>
+            <Nav />
+            <Overlay v-if="open" @click="() => (open = false)" />
+            <button @click="() => (open = true)">
                 <Icon icon="charm:menu-hamburger" width="28" height="28" />
             </button>
         </Container>
@@ -39,35 +44,18 @@ header {
         align-items: center;
         height: 100px;
     }
+
+    .overlay {
+        display: none;
+        @include on-mobile {
+            display: block;
+        }
+    }
+
     h1 {
         font-size: 20px;
     }
 
-    nav {
-        ul {
-            display: flex;
-            gap: 60px;
-
-            padding: 10px 20px;
-            border-radius: 10px;
-            background: rgba($white, 0.4);
-            // TODO background blur
-
-            @include on-tablet {
-                gap: 40px;
-            }
-            @include on-mobile {
-                display: none;
-            }
-
-            li {
-                a {
-                    font-size: 16px;
-                    font-weight: 700;
-                }
-            }
-        }
-    }
     button {
         display: none;
         @include on-mobile {
