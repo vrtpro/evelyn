6b3e530031c6c1cd90a987182d48fda8b7c9e6e1
BayuDC <dev.bayudc@gmail.com>
2022-07-10 12:27:30 +0700

    chore: add icon component

---
+++ src/shared/Icon.vue
@@ -0,0 +1,17 @@
+<script>
+import { Icon } from '@iconify/vue';
+
+export default {
+    name: 'Icon',
+    props: {
+        name: String,
+    },
+    components: {
+        Iconify: Icon,
+    },
+};
+</script>
+
+<template>
+    <Iconify :icon="name" width="28" height="28" />
+</template>
