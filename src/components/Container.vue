d335a26752ffdccce27d3d70e57299605d1a4eb5
BayuDC <dev.bayudc@gmail.com>
2022-07-10 10:37:31 +0700

    feat: add container component

---
+++ src/components/Container.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'Container',
+};
+</script>
+
+<template>
+    <div class="container">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss">
+@import '../styles/mixin';
+.container {
+    max-width: 1024px;
+    margin: auto;
+    padding: 0 40px;
+
+    @include on-tablet {
+        padding: 0 30px;
+    }
+    @include on-mobile {
+        padding: 0 20px;
+    }
+}
+</style>
