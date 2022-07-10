3f2d280ccd6f9a7f8c728d33ac4955a23d6e9240
BayuDC <dev.bayudc@gmail.com>
2022-07-10 12:19:33 +0700

    chore: add overlay component

---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'Nav',
+};
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><router-link to="#">Home</router-link></li>
+            <li><router-link to="#">Lorem</router-link></li>
+            <li><router-link to="#">Ipsum</router-link></li>
+            <li><router-link to="#">About</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<style lang="scss">
+@import '../styles/variable';
+@import '../styles/mixin';
+nav {
+    ul {
+        display: flex;
+        gap: 60px;
+
+        padding: 10px 20px;
+        border-radius: 10px;
+        background: rgba($white, 0.4);
+        // TODO background blur
+
+        @include on-tablet {
+            gap: 40px;
+        }
+        @include on-mobile {
+            display: none;
+        }
+
+        li {
+            a {
+                font-size: 16px;
+                font-weight: 700;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/Overlay.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'Overlay',
+};
+</script>
+
+<template>
+    <div class="overlay">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss">
+@import '../styles/variable';
+.overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    background: rgba($black, 0.4);
+}
+</style>
