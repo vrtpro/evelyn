3c3ef1627f432d278591c430b14d4851c74b95a9
BayuDC <dev.bayudc@gmail.com>
2022-07-10 13:22:29 +0700

    feat: interact with navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,70 +1,22 @@
 <script>
-import Container from './Container.vue';
-import Overlay from './Overlay.vue';
-import Nav from './Nav.vue';
-
-import Icon from '../shared/Icon.vue';
+import HeaderDefault from './HeaderDefault.vue';
+import HeaderOverlay from './HeaderOverlay.vue';
 
 export default {
     name: 'Header',
     data() {
         return {
-            open: false,
+            overlay: false,
         };
     },
     components: {
-        Container,
-        Overlay,
-        Icon,
-        Nav,
+        HeaderDefault,
+        HeaderOverlay,
     },
 };
 </script>
 
 <template>
-    <header class="abc">
-        <Container>
-            <h1><router-link to="#">Evelyn Logo</router-link></h1>
-            <Nav />
-            <Overlay v-if="open" @click="() => (open = false)" />
-            <button @click="() => (open = true)">
-                <Icon name="charm:menu-hamburger" />
-            </button>
-        </Container>
-    </header>
+    <HeaderDefault @toggle="() => (overlay = true)" />
+    <HeaderOverlay @toggle="() => (overlay = false)" :render="overlay" />
 </template>
-
-<style lang="scss">
-@import '../styles/variable';
-@import '../styles/mixin';
-header {
-    .container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 100px;
-    }
-
-    .overlay {
-        display: none;
-        @include on-mobile {
-            display: block;
-        }
-    }
-
-    h1 {
-        font-size: 20px;
-    }
-
-    button {
-        display: none;
-        @include on-mobile {
-            display: block;
-        }
-    }
-
-    a {
-        color: $black;
-    }
-}
-</style>
---
+++ src/components/HeaderOverlay.vue
@@ -0,0 +1,54 @@
+<script>
+import Header from './HeaderDefault.vue';
+import Overlay from './Overlay.vue';
+
+export default {
+    name: 'HeaderOverlay',
+    props: {
+        render: false,
+    },
+    components: {
+        Header,
+        Overlay,
+    },
+};
+</script>
+
+<template>
+    <Overlay v-if="render" @click="() => $emit('toggle')">
+        <Header toggleIcon="eva:close-outline" />
+    </Overlay>
+</template>
+
+<style lang="scss">
+@import '../styles/variable';
+@import '../styles/mixin';
+.overlay {
+    display: none;
+    @include on-mobile {
+        display: block;
+    }
+
+    header {
+        border-radius: 0px 0px 20px 20px;
+        background: $pink;
+        .container {
+            padding: 20px;
+            height: auto;
+            gap: 20px;
+        }
+
+        nav {
+            display: block;
+            width: 100%;
+            ul {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                padding: 20px;
+                gap: 20px;
+            }
+        }
+    }
+}
+</style>
