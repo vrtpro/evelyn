7eae83e8eeeb04f4ce4acaa0bf0e786392f9ccaf
Gifaldy Azka <me@falcxxdev.ml>
2022-07-17 21:07:52 +0700

    feat: add data file for fan art page

---
+++ src/data/fanarts.ts
@@ -0,0 +1,46 @@
+const arts = [
+    {
+        img: 'https://pbs.twimg.com/media/FRmAB6yagAI0br0.jpg',
+        artist: {
+            username: '@ElaineCelestia',
+            url: 'https://twitterc.com/ElaineCelestia',
+        },
+    },
+    {
+        img: 'https://pbs.twimg.com/media/Eu9cQWIU4AYZGex.jpg',
+        artist: {
+            username: '@AyumiAika_',
+            url: 'https://twitter.com/AyumiAika_',
+        },
+    },
+    {
+        img: 'https://pbs.twimg.com/media/E44l9wLUcAcMSdz.jpg',
+        artist: {
+            username: '@Ikazu401',
+            url: 'https://twitter.com/Ikazu401',
+        },
+    },
+    {
+        img: 'https://pbs.twimg.com/media/FN96pDoaAAIuEQs.jpg',
+        artist: {
+            username: '@deidey16_',
+            url: 'https://twitter.com/deidey16_',
+        },
+    },
+    {
+        img: 'https://pbs.twimg.com/media/FRljGC3VsAAMgQK.jpg',
+        artist: {
+            username: '@Ikazu401',
+            url: 'https://twitter.com/Ikazu401',
+        },
+    },
+    {
+        img: 'https://pbs.twimg.com/media/FXfleEFacAAwCVb.jpg',
+        artist: {
+            username: '@MerySyafira48',
+            url: 'https://twitter.com/MerySyafira48',
+        },
+    },
+];
+
+export default arts;
