f36675ffeef9c892463201e973c7a801cea5a83c
Gifaldy Azka <me@falcxxdev.ml>
2022-07-17 21:05:53 +0700

    chore: require props for card data

--- src/components/ArtCard.vue
+++ src/components/ArtCard.vue
@@ -1,19 +1,35 @@
 <script lang="ts">
 export default {
     name: 'ArtCard',
+    props: {
+        img: {
+            type: String,
+            required: true,
+        },
+        artist: {
+            type: String,
+            required: true,
+        },
+        artistUrl: {
+            type: String,
+            required: true,
+        },
+    },
 };
 </script>
 
 <template>
     <div class="card">
-        <img src="https://pbs.twimg.com/media/FRmAB6yagAI0br0.jpg" alt="Art image" />
+        <img :src="img" alt="Art image" />
         <div class="desc">
-            <p class="artist">@elainecelestia</p>
+            <a :href="artistUrl">{{ artist }}</a>
         </div>
     </div>
 </template>
 
 <style lang="scss">
+@import '../styles/variable';
+
 .card {
     max-width: 360px;
     aspect-ratio: 3/ 2;
@@ -38,6 +54,18 @@ export default {
 
         background: rgba(#d9d9d9, 0.5);
         backdrop-filter: blur(6px);
+        a {
+            color: $black;
+            &:hover {
+                color: $black;
+            }
+            &:active {
+                color: $black;
+            }
+            &:visited {
+                color: $black;
+            }
+        }
     }
 }
 </style>
