b1065c384782f7d3af31af001bbe149b42b23512
Gifaldy Azka <me@falcxxdev.ml>
2022-07-17 21:08:10 +0700

    feat: finish fan art page

--- src/pages/FanArt.vue
+++ src/pages/FanArt.vue
@@ -1,21 +1,53 @@
 <script>
 import Container from '../components/Container.vue';
 import ArtCard from '../components/ArtCard.vue';
+import arts from '../data/fanarts.ts';
+
 export default {
     name: 'FanArt',
     components: {
         ArtCard,
         Container,
     },
+    data() {
+        return {
+            arts,
+        };
+    },
 };
 </script>
 
 <template>
-    <Container>
-        <ArtCard />
-    </Container>
+    <section class="fanart">
+        <Container>
+            <div class="gallery" v-for="art in arts">
+                <ArtCard :img="art.img" :artist="art.artist.username" :artistUrl="art.artist.url" />
+            </div>
+        </Container>
+    </section>
 </template>
 
 <style lang="scss">
 @import '../styles/variable';
+@import '../styles/mixin';
+
+.fanart {
+    overflow: hidden;
+    padding: 20px 0;
+
+    @include on-mobile {
+        padding: 20px 0;
+    }
+
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 20px;
+
+        @include on-mobile {
+            grid-template-columns: 1fr;
+            gap: 20px;
+        }
+    }
+}
 </style>
