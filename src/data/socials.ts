9a2f5ff35d1a4a0c3f262faddf862f62d4a690f8
Gifaldy Azka <me@falcxxdev.ml>
2022-07-18 19:52:51 +0700

    feat: add social links data

---
+++ src/data/socials.ts
@@ -0,0 +1,19 @@
+import Socials from '../types/data/socials';
+
+const socials: Socials = {
+    youtube: 'https://youtube.com/c/HaiHaloEpel',
+    twitter: 'https://twitter.com/EpelEvelyn',
+    facebook: 'https://facebook.com/haihalo.epel',
+    instagram: 'https://instagram.com/HaiHalo.Epel',
+    smule: 'https://smule.com/EpelEvelyn',
+    tiktok: 'https://tiktok.com/@HaiHaloEpel',
+    discord: 'https://bit.ly/HaiHaloServer',
+    trakteer: 'https://trakteer.id/HaiHaloEpel',
+    saweria: 'https://saweria.co/HaiHaloEpel',
+    karyakarsa: 'https://karyakarsa.com/HaiHaloEpel',
+    streamlabs: 'https://streamlabs.com/evelynvtuber/tip',
+    steam: 'https://steamcommunity.com/id/EpelEvelyn',
+    reality: 'https://reality.app/profile/6e0ccda3',
+};
+
+export default socials;
