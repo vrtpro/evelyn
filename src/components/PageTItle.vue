500c25d3483b5f137cd8462e891e2224642bc601
BayuDC <dev.bayudc@gmail.com>
2022-07-19 09:28:08 +0700

    feat: page title for fanart

---
+++ src/components/PageTItle.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'PageTitle',
+};
+</script>
+
+<template>
+    <div class="page-title">
+        <h1><slot /></h1>
+
+        <p><slot name="sub" /></p>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../styles/variable';
+.page-title {
+    color: $black;
+    margin-bottom: 20px;
+
+    h1 {
+        font-size: 40px;
+    }
+    p {
+        font-family: $fira-sans;
+        font-size: 18px;
+    }
+}
+</style>
--- src/pages/FanArt.vue
+++ src/pages/FanArt.vue
@@ -1,15 +1,16 @@
 <script>
 import Container from '../components/Container.vue';
+import PageTitle from '../components/PageTItle.vue';
 import ArtCard from '../components/ArtCard.vue';
 import ArtList from '../components/ArtList.vue';
 import arts from '../data/fanarts.ts';
-import ArtList from '../components/ArtList.vue';
 
 export default {
     name: 'FanArt',
     components: {
-        ArtCard,
         Container,
+        PageTitle,
+        ArtCard,
         ArtList,
     },
     data() {
@@ -23,6 +24,10 @@ export default {
 <template>
     <section class="fanart">
         <Container>
+            <PageTitle>
+                <template #default>Fan art</template>
+                <template #sub>Curated Fan-made art from <u>#PienEpelPaint</u> hashtag at Twitter</template>
+            </PageTitle>
             <ArtList>
                 <li v-for="art in arts">
                     <ArtCard :img="art.img" :artist="art.artist.username" :artistUrl="art.artist.url" />
