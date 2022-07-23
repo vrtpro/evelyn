24835bd1973943d641ca360aac3ba3cff0ab4d70
Gifaldy Azka <me@falcxxdev.ml>
2022-07-23 11:49:24 +0700

    feat: use loading component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
-<template>
-    <Header />
-    <router-view v-slot="{ Component }">
+<template ref="false">
+    <Loading v-if="show" />
+    <router-view v-else v-slot="{ Component }">
+        <Header />
         <transition name="fade" mode="out-in">
             <component :is="Component"></component>
         </transition>
+        <Footer />
     </router-view>
-    <Footer />
 </template>
 
 <script lang="ts">
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import Loading from './components/Loading.vue';
 
 export default {
     name: 'App',
     components: {
         Header,
         Footer,
+        Loading,
+    },
+    data() {
+        return {
+            show: true,
+        };
+    },
+    mounted() {
+        setTimeout(() => {
+            this.show = !this.show;
+        }, 2000);
     },
 };
 </script>
