eba641efe6169f8aba95baeeafd3bda56fcf5b2f
Gifaldy Azka <me@falcxxdev.ml>
2022-07-23 11:49:06 +0700

    feat: create loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,38 @@
+<script>
+import LoadEffect from '../shared/LoadEffect.vue';
+import Container from './Container.vue';
+
+export default {
+    name: 'Loading',
+    components: {
+        LoadEffect,
+        Container,
+    },
+};
+</script>
+
+<template>
+    <Container>
+        <div class="loading">
+            <LoadEffect class="loading-text">Loading</LoadEffect>
+        </div>
+    </Container>
+</template>
+
+<style lang="scss">
+@import '../styles/variable';
+.loading {
+    font-family: $poppins;
+    font-size: 36px;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 560px;
+    transition: 0.2s ease-in-out;
+    &-text {
+        font-family: $fira-sans;
+        font-weight: 700;
+    }
+}
+</style>
---
+++ src/shared/LoadEffect.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'LoadEffect',
+    props: {
+        stop: { type: Boolean, default: false },
+    },
+};
+</script>
+
+<template>
+    <span class="load-effect" :class="{ stop }">
+        <slot />
+    </span>
+</template>
+
+<style lang="scss">
+.load-effect {
+    &::after {
+        content: '';
+        animation: 1.2s loading infinite;
+    }
+    &.stop::after {
+        animation: none;
+    }
+}
+@keyframes loading {
+    25% {
+        content: '';
+    }
+    50% {
+        content: '.';
+    }
+    75% {
+        content: '..';
+    }
+    100% {
+        content: '...';
+    }
+}
+</style>
