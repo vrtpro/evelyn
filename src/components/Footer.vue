8bcdc5e01cbc90641684596cba09343e618f6a4f
Gifaldy Azka <me@falcxxdev.ml>
2022-07-23 09:59:37 +0700

    feat: add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import Container from './Container.vue';
+
+export default {
+    name: 'Footer',
+    components: {
+        Container,
+    },
+};
+</script>
+
+<template>
+    <footer>
+        <p>Made with ❤️ by <b>Gifaldy Azka</b>.</p>
+    </footer>
+</template>
+
+<style lang="scss">
+@import '../styles/variable';
+
+footer {
+    padding-top: 100px;
+    p {
+        font-family: $fira-sans;
+        font-size: 14px;
+        border-top: 1px solid $black;
+        padding: 10px 0;
+        text-align: center;
+    }
+}
+</style>
