02fe6107713ad50b37e21671d0e0f85394712182
Gifaldy Azka <me@falcxxdev.ml>
2022-07-24 19:24:33 +0700

    refactor: rewrite socials data

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,19 +1,71 @@
-import Socials from '../types/data/socials';
+import { Socials } from '../types/data/socials';
 
-const socials: Socials = {
-    youtube: 'https://youtube.com/c/HaiHaloEpel',
-    twitter: 'https://twitter.com/EpelEvelyn',
-    facebook: 'https://facebook.com/haihalo.epel',
-    instagram: 'https://instagram.com/HaiHalo.Epel',
-    smule: 'https://smule.com/EpelEvelyn',
-    tiktok: 'https://tiktok.com/@HaiHaloEpel',
-    discord: 'https://bit.ly/HaiHaloServer',
-    trakteer: 'https://trakteer.id/HaiHaloEpel',
-    saweria: 'https://saweria.co/HaiHaloEpel',
-    karyakarsa: 'https://karyakarsa.com/HaiHaloEpel',
-    streamlabs: 'https://streamlabs.com/evelynvtuber/tip',
-    steam: 'https://steamcommunity.com/id/EpelEvelyn',
-    reality: 'https://reality.app/profile/6e0ccda3',
-};
+const socials: Socials = [
+    {
+        name: 'YouTube',
+        username: 'Hai Halo Epel',
+        url: 'https://youtube.com/c/HaiHaloEpel',
+    },
+    {
+        name: 'Twitter',
+        username: 'EpelEvelyn',
+        url: 'https://twitter.com/EpelEvelyn',
+    },
+    {
+        name: 'Facebook',
+        username: 'haihalo.epel',
+        url: 'https://facebook.com/haihalo.epel',
+    },
+    {
+        name: 'Instagram',
+        username: 'haihalo.epel',
+        url: 'https://instagram.com/HaiHalo.Epel',
+    },
+    {
+        name: 'Smule',
+        username: 'EpelEvelyn',
+        url: 'https://smule.com/EpelEvelyn',
+    },
+    {
+        name: 'TikTok',
+        username: 'HaiHaloEpel',
+        url: 'https://tiktok.com/@HaiHaloEpel',
+    },
+    {
+        name: 'Discord',
+        username: '💙 Piolet Eplegorden 💙',
+        url: 'https://bit.ly/HaiHaloServer',
+    },
+    {
+        name: 'Trakteer',
+        username: 'HaiHaloEpel',
+        url: 'https://trakteer.id/HaiHaloEpel',
+    },
+    {
+        name: 'Saweria',
+        username: 'HaiHaloEpel',
+        url: 'https://saweria.co/HaiHaloEpel',
+    },
+    {
+        name: 'Karyakarsa',
+        username: 'HaiHaloEpel',
+        url: 'https://karyakarsa.com/HaiHaloEpel',
+    },
+    {
+        name: 'Streamlabs',
+        username: 'evelynvtuber',
+        url: 'https://streamlabs.com/evelynvtuber/tip',
+    },
+    {
+        name: 'Steam',
+        username: 'EpelEvelyn',
+        url: 'https://steamcommunity.com/id/EpelEvelyn',
+    },
+    {
+        name: 'Reality',
+        username: 'Epel || Evelyn',
+        url: 'https://reality.app/profile/6e0ccda3',
+    },
+];
 
 export default socials;
